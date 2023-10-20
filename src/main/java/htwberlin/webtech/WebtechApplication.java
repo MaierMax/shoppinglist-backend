19dc6315762589806e77d8ed81abf3d69cf65d42
MaierMax <maier.max2004@gmail.com>
2023-10-20 13:49:41 +0200

    Hello World

--- src/main/java/htwberlin/webtech/WebtechApplication.java
+++ src/main/java/htwberlin/webtech/WebtechApplication.java
@@ -2,12 +2,21 @@ package htwberlin.webtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebtechApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebtechApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String getHello(){
+		return "Hallo aus Java";
 	}
 
 }
